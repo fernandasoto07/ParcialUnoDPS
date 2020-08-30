50ae81e0225ae152ba8f9bc6fe65fba285e8f7cf
JLopzz <javylopzz@gmail.com>
2020-08-30 15:45:39 -0600

    funcionamiento de clase de conexion

--- parcial1/src/app/app.module.ts
+++ parcial1/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { FormsModule } from '@angular/forms';
+//import { HttpClient } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
